37163894fa26b4b2784d54064e08e5fc0f07b8bf
YIPG <around.mr.child@gmail.com>
2019-04-14 18:20:27 +0900

    preact settings

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,1 +1,9 @@
-console.log("hello world");
+import { h, render } from "preact";
+
+render(
+  <div id="foo">
+    <span>Hello, world!</span>
+    <button onClick={e => alert("hi!")}>Click Me</button>
+  </div>,
+  document.body
+);
