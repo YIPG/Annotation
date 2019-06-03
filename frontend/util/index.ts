3c0eb1801c06827118071b27486e3582ca9c5b7f
YIPG <around.mr.child@gmail.com>
2019-06-03 19:03:39 +0900

    deleted cache

--- frontend/util/index.ts
+++
@@ -1,3 +0,0 @@
-export const getPercentage = (a, b) => {
-  return Math.floor((a / b) * 100);
-};
--- server/util/index.ts
+++
@@ -1,3 +0,0 @@
-export const isEqualPoint = (p1, p2) => {
-  return p1.x === p2.x && p1.y === p2.y;
-};
