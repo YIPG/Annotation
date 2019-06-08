dddb0e3fe9e9b16169d5d0e8b5519f54fc2d4fe1
yuya <u0ya317@gmail.com>
2019-06-08 14:20:23 +0900

    connect to DB

--- server/app.ts
+++ server/app.ts
@@ -8,6 +8,10 @@ app.use(cors());
 app.use(express.static("public"));
 
 // Check DB Connection
+
+mongoose.connect("mongodb://127.0.0.1:27017/annotation", {
+  useNewUrlParser: true
+});
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => console.log("connection OK"));
